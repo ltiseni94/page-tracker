ddaa6b3406205c454d83c130ed91fa3c8ce70f9a
Luca Tiseni <72504148+ltiseni94@users.noreply.github.com>
2024-08-03 19:00:32 +0200

    Replace the emoji in an error message (#1)
    * Replace the emoji in an error message
    
    * Fix the failing unit test

--- web/src/page_tracker/app.py
+++ web/src/page_tracker/app.py
@@ -16,7 +16,7 @@ def index():
         page_views = get_redis().incr("page_views")
     except RedisError:
         app.logger.exception("Redis error")
-        return "Sorry, something went wrong \N{pensive face}", 500
+        return "Sorry, something went wrong \N{thinking face}", 500
     return f"This page has been seen {page_views} times."
 
 
