46f11fac56fc1aa10994e711f03d884093618728
ltiseni94 <luca13tiseni@gmail.com>
2024-08-03 18:58:08 +0200

    Fix the failing unit test

--- web/test/unit/test_app.py
+++ web/test/unit/test_app.py
@@ -18,5 +18,5 @@ def test_should_handle_redis_connection_error(mock_redis, http_client):
     mock_redis.return_value.incr.side_effect = ConnectionError
     response = http_client.get("/")
     assert response.status_code == 500
-    assert response.text == "Sorry, something went wrong \N{pensive face}"
+    assert response.text == "Sorry, something went wrong \N{thinking face}"
 
